00482a2b9048264a3a27f759c3f7550443e8d98a
himanshi.n97 <himanshi.n97@gmail.com>
2020-09-11 15:44:47 +0530

    Updates

---
+++ precache-manifest.a91af01021c728b4cabc8c791610183e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25029c8de2875fd21c4a6d1201c8867a",
+    "url": "/User-Details-Form-App/index.html"
+  },
+  {
+    "revision": "18447820c666d12cec24",
+    "url": "/User-Details-Form-App/static/css/main.2face891.chunk.css"
+  },
+  {
+    "revision": "5caf4ab81c21bcfc8a5c",
+    "url": "/User-Details-Form-App/static/js/2.bb373a1c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/User-Details-Form-App/static/js/2.bb373a1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18447820c666d12cec24",
+    "url": "/User-Details-Form-App/static/js/main.3797d55a.chunk.js"
+  },
+  {
+    "revision": "1b3289e5f3f3404b48e5",
+    "url": "/User-Details-Form-App/static/js/runtime-main.7dfa9f94.js"
+  }
+]);
